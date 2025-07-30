ff0dc568b8a537636cb539a7e57f78c2f148c326
Edward Akpa Junior <akpaedwardjnr@gmail.com>
2025-07-30 03:45:00 -0700

    Create githubService.js

---
+++ github-user-search/src/services/githubService.js
@@ -0,0 +1,19 @@
+import axios from 'axios';
+
+const BASE_URL = 'https://api.github.com/users';
+
+/**
+ * Fetch GitHub user data by username.
+ * 
+ * @param {string} username - GitHub username to search.
+ * @returns {Promise<Object>} - GitHub user data.
+ * @throws Will throw an error if the request fails.
+ */
+export const fetchUserData = async (username) => {
+  try {
+    const response = await axios.get(`${BASE_URL}/${username}`);
+    return response.data;
+  } catch (error) {
+    throw error;
+  }
+};
