acb5489467ea60a77f60a92c67df64aece40c1e0
Edward Akpa Junior <akpaedwardjnr@gmail.com>
2025-07-30 04:27:25 -0700

    Update githubService.js

--- github-user-search/src/services/githubService.js
+++ github-user-search/src/services/githubService.js
@@ -1,10 +1,10 @@
 // src/services/githubService.js
 import axios from 'axios';
 
-export const fetchUserData = async (username) => {
+export const fetchUserData = async (query) => {
   try {
-    const response = await axios.get(`https://api.github.com/users/${username}`);
-    return response.data;
+    const response = await axios.get(`https://api.github.com/search/users?q=${query}`);
+    return response.data.items; // array of user objects
   } catch (error) {
     throw error;
   }
