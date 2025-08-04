46a084a25f3c3df0d00775a75667a02b97ab4eb4
Edward Akpa Junior <akpaedwardjnr@gmail.com>
2025-08-04 02:13:21 -0700

    Delete tailwind-react-integration directory

--- tailwind-react-integration/src/App.jsx
+++
@@ -1,9 +0,0 @@
-import UserProfile from './components/UserProfile';
-
-export default function App() {
-  return (
-    <div className="min-h-screen bg-blue-50 flex items-center justify-center">
-      <UserProfile />
-    </div>
-  );
-}
