10f2bbbc04037f939602283ef1c49ca63e948402
Edward Akpa Junior <akpaedwardjnr@gmail.com>
2025-08-04 01:59:20 -0700

    Add files via upload

---
+++ tailwind-react-integration/src/App.jsx
@@ -0,0 +1,9 @@
+import UserProfile from './components/UserProfile';
+
+export default function App() {
+  return (
+    <div className="min-h-screen bg-blue-50 flex items-center justify-center">
+      <UserProfile />
+    </div>
+  );
+}
