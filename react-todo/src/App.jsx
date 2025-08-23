3f9d2decc352c7c5b07d7416b082dff26844fe75
Edward Akpa Junior <akpaedwardjnr@gmail.com>
2025-08-23 10:40:23 -0700

    Update App.jsx

--- react-todo/src/App.jsx
+++ react-todo/src/App.jsx
@@ -1,3 +1,18 @@
 // src/App.jsx
-import React, { useState } from "react";
-import TodoList from "./components
+import React from "react";
+import TodoList from "./components/TodoList";
+import AddTodoForm from "./components/AddTodoForm";
+import TestingComponent from "./components/TestingComponent";
+
+export default function App() {
+  return (
+    <div className="app-container">
+      <h1>React Todo App</h1>
+      <AddTodoForm />
+      <TodoList />
+      
+      {/* ✅ Add Testing Component */}
+      <TestingComponent />
+    </div>
+  );
+}
