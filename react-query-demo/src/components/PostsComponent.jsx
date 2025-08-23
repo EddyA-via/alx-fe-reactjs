92a74dc1cce04da118847497b95e6eccd5c345ce
Edward Akpa Junior <akpaedwardjnr@gmail.com>
2025-08-23 09:12:38 -0700

    Create PostsComponent.jsx

---
+++ react-query-demo/src/components/PostsComponent.jsx
@@ -0,0 +1,27 @@
+import { useQuery } from "react-query";
+
+const fetchPosts = async () => {
+  const res = await fetch("https://jsonplaceholder.typicode.com/posts");
+  return res.json();
+};
+
+export default function PostsComponent() {
+  const { data, error, isLoading, refetch } = useQuery("posts", fetchPosts);
+
+  if (isLoading) return <p>Loading...</p>;
+  if (error) return <p>Error fetching posts</p>;
+
+  return (
+    <div>
+      <h2>Posts</h2>
+      <button onClick={() => refetch()}>Refetch</button>
+      <ul>
+        {data.slice(0, 5).map((post) => (
+          <li key={post.id}>
+            <b>{post.title}</b>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+}
