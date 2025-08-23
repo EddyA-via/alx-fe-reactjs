e65b1be1b456d14076631227aa4304fcd55ab66b
Edward Akpa Junior <akpaedwardjnr@gmail.com>
2025-08-23 10:08:37 -0700

    Create AuthContext.jsx

---
+++ react-router-advanced/src/context/AuthContext.jsx
@@ -0,0 +1,22 @@
+// src/context/AuthContext.jsx
+import React, { createContext, useContext, useState } from "react";
+
+const AuthContext = createContext();
+
+export function AuthProvider({ children }) {
+  const [user, setUser] = useState(null);
+
+  const login = (username) => setUser({ username });
+  const logout = () => setUser(null);
+
+  return (
+    <AuthContext.Provider value={{ user, login, logout }}>
+      {children}
+    </AuthContext.Provider>
+  );
+}
+
+// ✅ Custom hook
+export function useAuth() {
+  return useContext(AuthContext);
+}
