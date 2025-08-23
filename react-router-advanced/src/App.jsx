490e0b88219b56c4f7fb1984f36cb2c0462e8458
Edward Akpa Junior <akpaedwardjnr@gmail.com>
2025-08-23 09:14:11 -0700

    Create App.jsx

---
+++ react-router-advanced/src/App.jsx
@@ -0,0 +1,38 @@
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+import Home from "./components/Home";
+import Profile from "./components/Profile";
+import ProfileDetails from "./components/ProfileDetails";
+import ProfileSettings from "./components/ProfileSettings";
+import Login from "./components/Login";
+
+const isAuthenticated = false; // toggle this to test
+
+function ProtectedRoute({ children }) {
+  return isAuthenticated ? children : <Navigate to="/login" />;
+}
+
+export default function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+
+        <Route path="/login" element={<Login />} />
+
+        <Route
+          path="/profile"
+          element={
+            <ProtectedRoute>
+              <Profile />
+            </ProtectedRoute>
+          }
+        >
+          <Route path="details" element={<ProfileDetails />} />
+          <Route path="settings" element={<ProfileSettings />} />
+        </Route>
+
+        <Route path="/post/:id" element={<h2>Dynamic Post Page</h2>} />
+      </Routes>
+    </BrowserRouter>
+  );
+}
